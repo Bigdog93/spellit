017a116da698c906439c69db5b3b40533aeb0bc0
lkc263 <lkc263@naver.com>
2023-03-08 14:03:24 +0900

    [be-feat] 백엔드 프로젝트 생성

---
+++ spellit/src/main/java/com/urs/spellit/SpellitApplication.java
@@ -0,0 +1,13 @@
+package com.urs.spellit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellitApplication.class, args);
+	}
+
+}
