d8b2eebc612b377e1ecdb7942ef49c4a3ecb86f5
이주찬 <ljc9393@nate.com>
2023-03-20 16:52:04 +0900

    Revert "Merge branch 'leejuchan' into 'main'"
    This reverts commit cb1b3946122450456a916e2e74323e93e9d76393, reversing
    changes made to 1329b5978bc07f2db83a0a80d28b168937c9c71c.

--- spellit/src/main/java/com/urs/spellit/member/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.urs.spellit.member;
-
-import com.urs.spellit.member.model.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-}
