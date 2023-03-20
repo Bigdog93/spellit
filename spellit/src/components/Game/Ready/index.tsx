df2ddb11fc44537168f11a6622856b1b3d34733e
Chaeeun Lee <chaen511@naver.com>
2023-03-20 17:59:05 +0900

    [fe-feat] ?

--- spellit/src/components/Game/Ready/index.tsx
+++ spellit/src/components/Game/Ready/index.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react"
-import { useSelector, useDispatch, Provider } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { costActions } from '../../../store/cost';
-
-import './index.css'
-import Frame from '../../../assets/ui/Frame.png'
 import { RootState } from "@/store";
 
+import Frame from '../../../assets/ui/Frame.png'
+import './index.css'
+
 const Ready = () => {
-  
+
   // 나의 덱 정보 저장
+  // store/user.tsx의 정보 업뎃 및 연동 필요
   const myCards = ['Fire1', 'Light1', 'Ice1', 'Wind1', 'Dark1']
   
 
-  // cost 부족할 때 shake 효과
+  // cost 부족할 때 나타는 shake 효과
   const [isShaking, setIsShaking] = useState(false);
 
   function shake() {
