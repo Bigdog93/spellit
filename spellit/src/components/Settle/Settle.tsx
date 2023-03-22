0c4c6741e57ef538bc4bd914dc28557433b53c12
강인주 <gij452@kakao.com>
2023-03-22 15:58:24 +0900

    [fe-fix] settle ing

---
+++ spellit/src/components/Settle/Settle.tsx
@@ -0,0 +1,14 @@
+import react from "react";
+import "./Settle.css";
+
+function Settle() {
+    return (
+        <>
+            <div className="hp-box">
+                
+            </div>
+        </>
+    )
+}
+
+export default Settle;
