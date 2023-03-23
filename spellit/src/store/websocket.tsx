f924540b23d3c55a214921dab3c2fd13a8503acf
Chaeeun Lee <chane511@naver.com>
2023-03-23 23:32:02 +0900

    [fe-feat]socket

--- spellit/src/store/websocket.tsx
+++ spellit/src/store/websocket.tsx
@@ -1,53 +1,7 @@
-// import { createSlice } from '@reduxjs/toolkit';
-// import type { PayloadAction } from '@reduxjs/toolkit'
-// import useRef from 'react';
-
-// // type sessionType = {
-// //   ws: {
-// //     new (url: string | URL, protocols?: string | string[] | undefined): WebSocket;
-// //     prototype: WebSocket;
-// //     readonly CLOSED: number;
-// //     readonly CLOSING: number;
-// //     readonly CONNECTING: number;
-// //     readonly OPEN: number;
-// //   }
-// // }
-
-// const sessionState = {
-//   ws: WebSocket.prototype,
-// };
-
-// const sessionSlice = createSlice({
-//   name: 'session',
-//   initialState: sessionState,
-//   reducers: {
-//     connect(state) {
-//       console.log("connecting");
-//       state.ws = new WebSocket(`ws://localhost:8080/api/socket`);
-//       state.ws.onopen = () => {
-//         state.ws.onmessage = (e :MessageEvent) => {
-//           console.log(e);
-//           const content = JSON.parse(e.data);
-//           console.log(content);
-//         }
-//       }
-//     },
-//     send(state, action: PayloadAction<Object>) {
-//       console.log("send 호출 됨");
-//       console.log(action);
-//       state.ws.send(JSON.stringify(action.payload));
-//     }
-//   },
-// });
-
-// export const sessionActions = sessionSlice.actions;
-
-// export default sessionSlice.reducer;
-
 import { createContext, useRef } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import { costActions } from "@/store/cost"
-import { attackActions } from "@/store/attack"
+// import { attackActions } from "@/store/attack"
 import { useDispatch } from 'react-redux';
 
 const WebSocketContext = createContext<any>(null);
@@ -56,6 +10,7 @@ export { WebSocketContext };
   
 // eslint-disable-next-line import/no-anonymous-default-export
 export const WebSocketProvider =  ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate()
   const webSocketUrl = `ws://localhost:8080/api/socket`
   let ws = useRef<WebSocket | null>(null).current;
   let send = ws?.send;
@@ -77,10 +32,42 @@ export const WebSocketProvider =  ({ children }: { children: React.ReactNode }) 
     ws.onmessage = (e) => {
       console.log(e);
       const content = JSON.parse(e.data);
+      const type = content.type;
       const info = content.info;
-      
-      dispatch(costActions.set(info.data));
-      
+      if (type === 'test') {
+        console.log('test입니다.')
+        dispatch(costActions.set(info.data));
+        navigate('/home')
+      } else if (type === 'entQueue') {
+        console.log('entQueue 입니다.')
+
+      } else if (type === 'loading') {
+        console.log('loading 입니다.')
+
+      } else if (type === 'toReady') {
+        console.log('toReady 입니다.')
+
+      } else if (type === 'toAttack') {
+        console.log('toAttack 입니다.')
+
+      } else if (type === 'otherSpell') {
+        console.log('otherSpell 입니다.')
+
+      } else if (type === 'combo') {
+        console.log('combo 입니다.')
+
+      } else if (type === 'toDefense') {
+        console.log('toDefense 입니다.')
+
+      } else if (type === 'toSettle') {
+        console.log('toSettle 입니다.')
+
+      } else if (type === 'gameOver') {
+        console.log('gameOver입니다.')
+        
+      } else {
+        console.log('그런 이벤트는 없습니다.')
+      }
     }
 
     send = (data: any) => {
