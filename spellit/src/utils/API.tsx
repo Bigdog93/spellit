5492b56b4d4604f376b650cb8dea137215f1744e
Chaeeun Lee <chane511@naver.com>
2023-03-25 00:21:31 +0900

    [fe-feat]API URL

--- spellit/src/utils/API.tsx
+++ spellit/src/utils/API.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_SPRING,
+    baseURL: 'https://j8d201.p.ssafy.io/api/',
+    // baseURL: process.env.REACT_APP_SPRING,
     headers: {
       "Content-Type": "application/json",
     },
