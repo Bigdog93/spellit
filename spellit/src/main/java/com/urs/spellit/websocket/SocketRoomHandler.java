ef0d5c6c1b21aa1df7ab31cb897a949db7fb7e7f
이주찬 <ljc9393@nate.com>
2023-03-26 21:58:29 +0900

    [be-test] openvidu port 8443 -> 8442

--- spellit/src/main/java/com/urs/spellit/websocket/SocketRoomHandler.java
+++ spellit/src/main/java/com/urs/spellit/websocket/SocketRoomHandler.java
@@ -151,7 +151,8 @@ public class SocketRoomHandler extends TextWebSocketHandler {
 					}
 					break;
 				case "spell": // 주문 외우면 외운 주문 상대에게
-					other.getSession().sendMessage(makeTextMsg("otherSpell", data));
+					infoMap.put("spell", myParser.getBackData(data));
+					other.getSession().sendMessage(makeTextMsg("otherSpell", infoMap));
 					break;
 				case "combo": // 100% 달성 시 상대에게 콤보 발동을 알림
 					other.getSession().sendMessage(makeTextMsg("combo", infoMap));
