6b168cd1a847fe9c5fbb8910f5c9e54299fca653
이재완 <jwlee2750@naver.com>
2023-03-29 13:27:00 +0900

    [BE-fix] remove duplicate allargsconstructor

--- spellit/src/main/java/com/urs/spellit/member/model/entity/Friend.java
+++ spellit/src/main/java/com/urs/spellit/member/model/entity/Friend.java
@@ -11,7 +11,6 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @RequiredArgsConstructor
-@AllArgsConstructor
 public class Friend {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
