c4a2e0d6c9ea070419675fb39fc2c95a13b0a96e
foresec <foresec@naver.com>
2023-03-30 11:12:16 +0900

    [fe-fix] profile 이동

--- spellit/src/App.tsx
+++ spellit/src/App.tsx
@@ -6,18 +6,18 @@ import { RootState } from "./store";
 import Home from "./components/Home";
 import Game from "./components/Game";
 import Matching from "./components/Matching";
-import User from './components/User'
-import Ready from "@/components/Game/Ready"
-import Defence from '@/components/Game/Defense/Defense';
+import User from "./components/User";
+import Ready from "@/components/Game/Ready";
+import Defence from "@/components/Game/Defense/Defense";
 import Attack from "@/components/Game/Attack";
 import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 import OAuth from "./components/Auth/OAuth";
 import Settle from "./components/Settle/Settle";
 import Result from "./components/Game/Result";
-import Deck from "./components/User/index"
-import Skills from "@/components/Settle/Skills"
-
+import Deck from "./components/User/index";
+import Skills from "@/components/Settle/Skills";
+import Profile from "./components/Profile";
 function App() {
   const isLogged = useSelector(
     (state: RootState) => state.auth.isAuthenticated
@@ -38,15 +38,16 @@ function App() {
           <Route path="matching" element={<Matching />} />
           <Route path="deck" element={<Deck />} />
           <Route path="ready" element={<Ready />} />
-          <Route path="defense" element={<Defence/>}/>
-          <Route path="attack" element={<Attack/>}/>
-          <Route path="login" element={<Login/>}/>
-          <Route path="oath" element={<OAuth/>}/>
-          <Route path="join" element={<Signup/>}/>
+          <Route path="defense" element={<Defence />} />
+          <Route path="attack" element={<Attack />} />
+          <Route path="login" element={<Login />} />
+          <Route path="oath" element={<OAuth />} />
+          <Route path="join" element={<Signup />} />
+          <Route path="profile/:id" element={<Profile />} />
           {/* <Route path="test" element={<Test/>}/> */}
-          <Route path="settle" element={<Settle/>}/>
-          <Route path="result" element={<Result/>}/>
-					<Route path="skills"element={<Skills/>}/>
+          <Route path="settle" element={<Settle />} />
+          <Route path="result" element={<Result />} />
+          <Route path="skills" element={<Skills />} />
         </Routes>
       </BrowserRouter>
     </WebSocketProvider>
