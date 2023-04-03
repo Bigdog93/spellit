a5e136ce2bb11bc6edf884dc9d591767704e06cd
Chaeeun Lee <chaen511@naver.com>
2023-04-03 16:30:59 +0900

    [fe-feat]deck

--- spellit/src/components/User/Cards.tsx
+++ spellit/src/components/User/Cards.tsx
@@ -1,3 +1,8 @@
+import {RootState} from '@/store'
+import { useSelector } from 'react-redux';
+
+import _ from 'lodash'
+
 import Card from './Card'
 import style from './index.module.css'
 
@@ -20,15 +25,33 @@ const Cards = ({cards, selectCard}: PropsType) => {
     selectCard(data);
     console.log(data)
   }
- 
+  
+  const myDeck = useSelector((state: RootState) => (state.user.deck))
+  console.log('myDeck ', myDeck)
+  // const mine = cards.filter(c => myDeck.includes(c))
+  const mine = cards.filter(c => myDeck.includes(c))
+  const notMine = cards.filter(c=> !(myDeck.includes(c)))
+  console.log('mine ', mine)
+  console.log('notMine ', notMine)
+
   return (
     <div className={`${style.cardItems}`}>
-      { cards.map((card: CardType, index: number) => (
+      { mine.map((card: CardType, index: number) => (
         <div onClick={(e) => onSelectCard(card)} className={`${style.cardContainer}`}>
           <Card key={index} card={card.code}/>
         </div>
       ))}
-      
+      <hr />
+      { notMine.map((card: CardType, index: number) => (
+        <div onClick={(e) => onSelectCard(card)} className={`${style.cardContainer}`}>
+          <Card key={index} card={card.code}/>
+        </div>
+      ))}
+      {/* { cards.map((card: CardType, index: number) => (
+        <div onClick={(e) => onSelectCard(card)} className={`${style.cardContainer}`}>
+          <Card key={index} card={card.code}/>
+        </div>
+      ))} */}
     </div>
   )
 }
