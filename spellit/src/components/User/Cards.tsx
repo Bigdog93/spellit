58f39e11b5a3c9ac8eb4caa39bb693d6172be58b
Chaeeun Lee <chane511@naver.com>
2023-04-04 01:59:07 +0900

    [fe-feat]deck flip sound

--- spellit/src/components/User/Cards.tsx
+++ spellit/src/components/User/Cards.tsx
@@ -1,11 +1,10 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 // import { useEffect } from 'react';
+import useSound from 'use-sound';
 import { useSelector } from 'react-redux';
 import _ from 'lodash'
-
 import { RootState } from '@/store'
 import { DeckType } from '@/utils/Types';
-
 import Card from './Card'
 import style from './index.module.css'
 
@@ -29,48 +28,85 @@ const Cards = ({cards, selectCard}: PropsType) => {
     console.log(data)
   }
   
-  const myDeck = useSelector((state: RootState) => (state.user.deck))
+  // const myDeck = useSelector((state: RootState) => (state.user.deck))
 
+  // const [myDeckId, setMyDeckId] = useState<Array<number>>([]);
+  // useEffect(() => {
+  //   for (const m of myDeck) {
+  //     setMyDeckId([...myDeckId, m.id])
+  //     // myDeckId.push(m.id)
+  //   }
+  // }, myDeck)
   // const mine = cards.filter(c => myDeck.includes(c))
   // const [notMine, setNotMine] = useState<Array<DeckType>>([]);
-  let notMine = useRef<Array<DeckType>>([]);
+  // let notMine = useRef<Array<DeckType>>([]);
 
-  useEffect(() => {
-    for (const card of cards) {
-      for (const m of myDeck) {
-        console.log(m.title)
-        if (card.id !== m.id) {
+  // useEffect(() => {
+  //   notMine.current = []
+  //   for (const card of cards) {
+  //     for (const m of myDeck) {
+  //       console.log(m.title)
+  //       if (card.id !== m.id) {
+  //         notMine.current.push(card)
+  //         console.log(notMine)
+  //       } else {
+  //         const index = notMine.current.indexOf(card)
+  //         notMine.current.slice(0, index).concat(notMine.current.slice(index + 1))
+  //       }
+  //       break
+  //     }
+  //   }
+  // }, [myDeck])
 
-          notMine.current.push(card)
-          // setNotMine([...notMine, card])
-          console.log(notMine)
-        }
-        break
-      }
-    }
-  }, [myDeck])
+  // const [notMine, setNotMine] = useState<Array<DeckType>>([]);
 
-  console.log('myDeck ', myDeck)
-  console.log('notMine ', notMine)
+  // useEffect(() => {
+  
+  //   for (const card of cards) {
+  //     for (const m of myDeck) {
+  //       console.log(m.title)
+  //       if (card.id !== m.id) {
+  //         setNotMine([...notMine, card])
+  //         // notMine.push(card)
+  //         console.log(notMine)
+  //       } else {
+  //         const index = notMine.indexOf(card)
+  //         notMine.slice(0, index).concat(notMine.slice(index + 1))
+  //       }
+  //       break
+  //     }
+  //   }
+  // }, [myDeck])
 
+  // console.log('myDeck ', myDeck)
+  // console.log('notMine ', notMine)
+
+
+  // CardFlip Sound Effect
+  const CardFlip = require("../../assets/soundeffect/CardFlip.mp3");
+  const [playFlip, {stop}] = useSound(CardFlip)
   return (
     <div className={`${style.cardItems}`}>
-      { myDeck.map((card: CardType, index: number) => (
+      {/* { myDeck.map((card: CardType, index: number) => (
         <div onClick={(e) => onSelectCard(card)} className={`${style.cardContainer}`}>
           <Card key={index} card={card.code}/>
         </div>
-      ))}
-      <hr />
+      ))} */}
       {/* { notMine.map((card: CardType, index: number) => (
         <div onClick={(e) => onSelectCard(card)} className={`${style.cardContainer}`}>
           <Card key={index} card={card.code}/>
         </div>
       ))} */}
-      {/* { cards.map((card: CardType, index: number) => (
+      {/* { notMine.current.map((card: CardType, index: number) => (
         <div onClick={(e) => onSelectCard(card)} className={`${style.cardContainer}`}>
           <Card key={index} card={card.code}/>
         </div>
       ))} */}
+      { cards.map((card: CardType, index: number) => (
+        <div onClick={(e) => onSelectCard(card)} className={`${style.cardContainer}`} onMouseEnter={() => playFlip()} onMouseLeave={() => stop()}>
+          <Card key={index} card={card.code}/>
+        </div>
+      ))}
     </div>
   )
 }
