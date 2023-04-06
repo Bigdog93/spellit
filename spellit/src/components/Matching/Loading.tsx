31e6ef0217999b4d0f8a27552977ee5c0812ac7a
Chaeeun Lee <chane511@naver.com>
2023-04-06 21:10:49 +0900

    [fe-feat]loading

--- spellit/src/components/Matching/Loading.tsx
+++ spellit/src/components/Matching/Loading.tsx
@@ -1,26 +1,37 @@
 import './Loading.css'
+import loadingBg from '../../assets/ui/loadingBg.png'
+import loadingCircle from '../../assets/ui/loadingCircle.png'
 
 const Loading = () => {
 
   return (
-    <div>
-       <h1>
-        <span>대</span>
-        <span>전</span>
-        <span> </span>
-        <span>상</span>
-        <span>대</span>
-        <span>를</span>
-        <span> </span>
-        <span>찾</span>
-        <span>는</span>
-        <span> </span>
-        <span>중</span>
-        <span>입</span>
-        <span>니</span>
-        <span>다</span>
-        <span>.</span>
-      </h1>
+    <div className='loading'>
+      <img src={loadingBg} alt="loadingBg" className='loadingBg' />
+      <div>
+        <h1>
+          <span>대</span>
+          <span>전</span>
+          <span>&nbsp;</span>
+          <span>상</span>
+          <span>대</span>
+          <span>를</span>
+          <span>&nbsp;</span>
+          <span>찾</span>
+          <span>는</span>
+          <span>&nbsp;</span>
+          <span>중</span>
+          <span>입</span>
+          <span>니</span>
+          <span>다</span>
+          <span>.</span>
+        </h1>
+        <br />
+        <br />
+        <br />
+        <br />
+        {/* <img src={loadingCircle} alt="loadingCircle" className="loadingCircle"/> */}
+      </div>
+
     </div>
   )
 }
